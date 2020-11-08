f4a4b39968ccd812548168720e5cf598261cbe47
akshayagrawal87 <akshayagrawal87@gmail.com>
2020-11-08 21:34:40 +0530

    added metaData

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
-const express = require("express");
+(() => {
+	const express = require("express");
 
-const app = express();
+	const app = express();
 
-const fs = require("fs");
+	const fs = require("fs");
 
-const location = "dataBase";
+	const location = "dataBase";
 
-const path = require("path");
+	const path = require("path");
 
-const port = process.env.PORT || 4000;
+	const port = process.env.PORT || 4000;
 
-app
-	.get("/createFile", (req, res) => {
-		fs.writeFile(
-			path.join(location, `${new Date()}.txt`),
-			`${new Date()}`,
-			function (err) {
-				if (err) throw err;
-				console.log("Saved!");
-			}
-		);
+	const fileData = [];
 
-		console.log(new Date());
-		res.status(200).send(new Date());
-	})
-	.listen(port);
+	app
+		.get("/", (req, res) => {
+			res
+				.status(200)
+				.send(
+					"<h1>Welcome to File System</h1><h2>Api 1: /createFile</h2><h2>Api 2: /getAllFiles</h2>"
+				);
+		})
+		.get("/createFile", (req, res) => {
+			fs.writeFile(
+				path.join(location, `${new Date()}.txt`),
+				`${new Date()}`,
+				function (err) {
+					if (err) throw err;
+					console.log("Saved!");
+				}
+			);
+
+			console.log(new Date());
+			res.status(200).send(new Date());
+		})
+		.get("/getAllFiles", (req, res) => {
+			fs.readdir(location, (err, files) => {
+				res.status(200).send(files);
+				files.forEach((file) => {
+					console.log(file);
+				});
+			});
+		})
+
+		.listen(port);
+})();
